8685a8bba5d46a9be3b6775cb67e26dbe0e3ac91
senhas-rgb <senhasdiwan@gmail.com>
2024-11-06 18:29:23 +0530

    struct is fixed??? ig

--- src/cpuinfo.h
+++ src/cpuinfo.h
@@ -6,7 +6,14 @@ typedef struct {
     char vendor[30];
     uint32_t model;
     uint32_t family;
+    uint32_t stepping;
+    uint32_t extended_model;
+    uint32_t extended_family;
+    uint32_t features;
+    uint32_t cache_size;
 } CPUinfo;
 
-void get_cpu_info(CPUinfo* cpu_info); 
-void print_cpuinfo();
+void get_cpu_info(CPUinfo* cpu_info);
+void print_cpuinfo(void);
+void print_detailed_cpu_info(void);
+void get_detailed_cpu_info(CPUinfo* cpu_info);
