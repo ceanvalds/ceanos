21bcab997c9f88ea614bc2f3fa6038b15ae3ebb9
senhas-rgb <senhasdiwan@gmail.com>
2024-11-06 17:46:04 +0530

    System Information Functions

--- src/cpuinfo.c
+++ src/cpuinfo.c
@@ -1,35 +1,26 @@
-#include <cpuinfo.h>
-#include <stdlib/stdio.h>
+#include "cpuinfo.h"
 
-void get_cpu_info(CPUinfo* cpu_info)
-{
+void get_detailed_cpu_info(CPUinfo* cpu_info) {
     uint32_t eax, ebx, ecx, edx;
-
-    asm volatile (
-        "cpuid"
+    
+    // Get vendor ID
+    asm volatile ("cpuid" 
+        : "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx) 
+        : "a"(0));
+        
+    *((uint32_t*)&cpu_info->vendor[0]) = ebx;
+    *((uint32_t*)&cpu_info->vendor[4]) = edx;
+    *((uint32_t*)&cpu_info->vendor[8]) = ecx;
+    cpu_info->vendor[12] = '\0';
+    
+    // Get processor info and feature bits
+    asm volatile ("cpuid"
         : "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
-        : "a"(0)
-    );
-
-    *((uint32_t*)&cpu_info->vendor[0]) = ebx;  // first 4 bytes
-    *((uint32_t*)&cpu_info->vendor[4]) = edx;  // next 4 bytes
-    *((uint32_t*)&cpu_info->vendor[8]) = ecx;  // last 4 bytes
-    cpu_info->vendor[12] = '\0';  // null-terminate the string
-
-    asm volatile (
-        "cpuid"
-        : "=a"(eax)
-        : "a"(1)
-    );
-
+        : "a"(1));
+        
     cpu_info->model = (eax >> 4) & 0xF;
     cpu_info->family = (eax >> 8) & 0xF;
-}
-
-inline void print_cpuinfo()
-{
-    CPUinfo cpu_info;
-    get_cpu_info(&cpu_info);
-
-    printf("CPU: %s | M: %u | F: %u\n", cpu_info.vendor, cpu_info.model, cpu_info.family);
-}
+    cpu_info->stepping = eax & 0xF;
+    cpu_info->extended_model = (eax >> 16) & 0xF;
+    cpu_info->extended_family = (eax >> 20) & 0xFF;
+} 
